d668d6a7b9d7c4f7b12f01f0532e39124f7ffecb
MD ALIF <alifkhan.bubt@gmail.com>
2023-12-18 18:09:25 +0600

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,20 @@
+import '@styles/global.css';
+export const metadata = {
+    title:'promtapia',
+    description:'Discover & Share AI Promts'
+}
+
+const layout = () => {
+  return (
+    <html lang='en'>
+        <body>
+            <div className='main'>
+                <div className='gradient' />
+            </div>
+        </body>
+
+    </html>
+  )
+}
+
+export default layout
